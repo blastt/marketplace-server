f167bf60b44f8f3e7351a9bf7e4cda9680ba6224
tanat <t.anatolii96@gmail.com>
2022-05-09 23:07:29 +0200

    main3

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!3223ee");
+app.MapGet("/", () => "Hello World!3223ssee");
 
 app.Run();
